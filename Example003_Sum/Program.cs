b390c68641c5ad161a35b530d03e692e02c3affd
Alex89417 <zezylia25@gmail.com>
2023-07-18 22:46:30 +0300

    Example 003 Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,16 @@
+﻿//int numberA = 3;
+//int numberB = 5;
+//Console.WriteLine(numberA + numberB);
+
+       //Выводит в консоль так:
+//8
+
+//---------------------------------------------------
+
+int numberA = 3;
+int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(result);
+
+       //Выводит в консоль так:
+//8
