79b7ed90418232d31c12f501a5590b1a875efdd4
Alex89417 <zezylia25@gmail.com>
2023-07-18 22:20:45 +0300

    Exemple 002 Hello User

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,21 @@
+﻿//Console.WriteLine("Введите Ваше имя");
+//string username = Console.ReadLine();
+//Console.WriteLine("Привет, ");
+//Console.WriteLine(username);
+
+       //Выводит в консоль так:
+//Введите Ваше имя
+//Александр
+//Привет, 
+//Александр
+//------------------------------------------------------------------
+
+Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
+
+
+       //Выводит в консоль так:
+//Введите Ваше имя: Александр
+//Привет, Александр
