341abc4b0b49ad295086cc2a951474a1923ed850
Alex89417 <zezylia25@gmail.com>
2023-07-18 22:59:39 +0300

    add Example 004 Div

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,15 @@
+﻿//int numberA = 12;
+//int numberB = 5;
+//Console.WriteLine(numberA / numberB);
+
+       //Выводит в консоль так:
+//2
+
+//-----------------------------------------
+
+double numberA = 12;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
+
+       //Выводит в консоль так:
+//2,4
