11a2a00864475ffa1e1c8fc224898603648126a1
Alex89417 <zezylia25@gmail.com>
2023-07-23 21:42:48 +0300

    2 лекция 25:06

---
+++ 2_Lecture/Examle002_IntroArray/Program.cs
@@ -0,0 +1,12 @@
+﻿// int Max(int arg1, int arg2, int arg3)
+// {
+//     int result = arg1;
+//     if (arg2 > result) result = arg2;
+//     if (arg3 > result) result = arg3;
+//     return result;
+// }
+
+// int[] array = {1,23,123,6,345,234,12,656,44};
+// int max = array[0];
+// int count = 0;
+// int index = [0];
