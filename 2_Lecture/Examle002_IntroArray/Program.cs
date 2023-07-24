6998a13fa739ce584459643484f6018f892029f5
Alex89417 <zezylia25@gmail.com>
2023-07-24 19:23:11 +0300

    add Examle 002 in folder 2 Lecture

--- 2_Lecture/Examle002_IntroArray/Program.cs
+++ 2_Lecture/Examle002_IntroArray/Program.cs
@@ -1,12 +1,16 @@
-﻿// int Max(int arg1, int arg2, int arg3)
-// {
-//     int result = arg1;
-//     if (arg2 > result) result = arg2;
-//     if (arg3 > result) result = arg3;
-//     return result;
-// }
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
 
-// int[] array = {1,23,123,6,345,234,12,656,44};
-// int max = array[0];
-// int count = 0;
-// int index = [0];
+int[] array = {1,23,1234,6,345,234,12,656,44};
+
+int result = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+    );
+Console.WriteLine(result);
