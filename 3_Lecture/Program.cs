63230e9e4cfbe93a76f76d135ba00b7a1ed703eb
Alex89417 <zezylia25@gmail.com>
2023-08-12 10:59:02 +0300

    complited 3 Lecture

--- 3_Lecture/Program.cs
+++ 3_Lecture/Program.cs
@@ -123,41 +123,7 @@ Console.WriteLine(newText);
 */
 
 
-// Task 2. Отсортировка элементов массива от меньшего к большему:
-
-int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
-
-void PrintArray(int[] array)
-{
-    int count = array.Length;
-
-    for (int i = 0; i < count; i++)
-    {
-        Console.Write(array[i] + " ");
-    }
-    Console.WriteLine();
-}
-
-void SelectionSort(int[] array)
-{
-    for (int i = 0; i < array.Length; i++) 
-    {
-        int minPosition = i;
-
-        for (int j = i + 1; j < array.Length; j++)
-        {
-            if(array[j] < array[minPosition]) minPosition = j;
-        }
-        int temporary = array[i];
-        array[i] = array[minPosition];
-        array[minPosition] = temporary;
-    }
-}
-
-PrintArray(arr);
-SelectionSort(arr);
-
-PrintArray(arr);
+/* Task 2. Отсортировка элементов массива от меньшего к большему:
 
 
 int[] Array = { 23, 255, 56, 41, 76, 83, 8 };
@@ -173,7 +139,7 @@ void PrintArr(int[] array)
 
 
 // метод сортировки стаканом
-/* void SelectArr(int[] array)
+void SelectArr(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
@@ -189,7 +155,7 @@ void PrintArr(int[] array)
         array[arrMin] = temp;
     }
 }
-*/
+
 
 
 // метод сортировки пузырьком
@@ -214,27 +180,6 @@ PrintArr(Array);
 SelectArr(Array);
 
 PrintArr(Array);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+*/
 
 
